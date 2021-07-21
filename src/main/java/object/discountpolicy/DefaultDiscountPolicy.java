37b9c301af3695abf6f89f623b70aa49089be174
jeon-ji-yoon <rebegin15@gmail.com>
2021-07-21 12:28:56 +0900

    refactor: 할인조건 리스트에서 addAll() 메서드 사용하여 요소를 추가하도록 바꿈

--- src/main/java/object/discountpolicy/DefaultDiscountPolicy.java
+++ src/main/java/object/discountpolicy/DefaultDiscountPolicy.java
@@ -12,7 +12,7 @@ public abstract class DefaultDiscountPolicy implements DiscountPolicy{
     private List<DiscountCondition> conditions = new ArrayList<>();
 
     public DefaultDiscountPolicy(DiscountCondition ... conditions) {
-        this.conditions = Arrays.asList(conditions);
+        this.conditions.addAll(Arrays.asList(conditions));
     }
 
     @Override
