c19930209c915e58dd599b453e59fb0406dd9bfd
jeon-ji-yoon <rebegin15@gmail.com>
2021-07-31 14:50:37 +0900

    refactor: 테스트 코드 수정

--- src/test/java/object/AppConfigTest.java
+++ src/test/java/object/AppConfigTest.java
@@ -8,11 +8,13 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AppConfigTest {
-    Screening screening;
+
 
     @Test
     void addScreeningSchedule() {
+        Screening screening;
         AppConfig appConfig = new AppConfig();
+
         screening = appConfig.screening();
         Customer customer1 = new Customer(1L, "nameA");
         Reservation reservation = screening.reserve(1, customer1);
